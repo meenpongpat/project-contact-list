fda6684450a59f7a066fc924603e3ab3c50b1d74
Neeracha <khanompeai2557@gmail.com>
2017-12-16 15:15:06 +0700

    Create function get

--- contacts/index.js
+++ contacts/index.js
@@ -15,6 +15,31 @@ var person = [
 ]
 
 //เขียน api สำหรับเรียกดูข้อมูล (get)
+router.get('/', (req, res) => {
+    
+     var x = req.query.firstname;
+      if(x != null)
+      {
+          for(var i=0; i<person.length; i++)
+          {
+              if(person[i].firstname == x)
+              {
+                  res.json(person[i]);
+              }   
+          }
+      }
+      else{
+          res.json(person);
+      }
+     
+  })
+  
+router.get('/:id', (req, res) => {
+    
+    res.json(person[req.params.id]);
+    
+ })
+
 //เขียน api สำหรับเพิ่มข้อมูล (post)
 //เขียน api สำหรับแก้ไขข้อมูล (put)
 //เขียน api สำหรับลบข้อมูล  (delete)
